142b6e22a114b9a11fd25791601daf36e345b47f
a-view <one_ouu@163.com>
2019-08-20 17:48:19 +0800

    项目大纲
    初步测试mqtt打通通信

---
+++ 01.website&server/mqttServer_Console/mqttServer.cs
@@ -0,0 +1,109 @@
+﻿using MQTTnet;
+using MQTTnet.Core.Adapter;
+using MQTTnet.Core.Diagnostics;
+using MQTTnet.Core.Protocol;
+using MQTTnet.Core.Server;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace mqttServer_Console
+{
+    public class mqttServer
+    {
+        private static MqttServer obj_mqttServer = null;//创建mqttServer对象
+
+        static void Main(string[] args)
+        {
+            MqttNetTrace.TraceMessagePublished += MqttNetTrace_TraceMessagePublished;
+            new Thread(StartMqttServer).Start();
+
+            while (true)
+            {
+                var inputString = Console.ReadLine().ToLower().Trim();
+
+                if (inputString == "exit")
+                {
+                    obj_mqttServer?.StopAsync();   //判断mqtt服务是否已经停止
+                    Console.WriteLine("MQTT服务已停止！");
+                    break;
+                }
+                else if (inputString == "clients")
+                {
+                    foreach (var item in obj_mqttServer.GetConnectedClients())//遍历连接到mqttServer的客户端
+                    {
+                        Console.WriteLine($"客户端标识：{item.ClientId}，协议版本：{item.ProtocolVersion}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"命令[{inputString}]无效！");
+                }
+            }
+        }
+
+        public static void StartMqttServer()
+        {
+            if (obj_mqttServer == null)//如果尚未创建mqtt服务器
+            {
+                try
+                {
+                    var options = new MqttServerOptions
+                    {
+                        ConnectionValidator = p =>
+                        {
+                            if (p.ClientId == "c001")
+                            {
+                                if (p.Username != "u001" || p.Password != "p001")
+                                {
+                                    return MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
+                                }
+                            }
+
+                            return MqttConnectReturnCode.ConnectionAccepted;
+                        }
+                    };
+
+                    obj_mqttServer = new MqttServerFactory().CreateMqttServer(options) as MqttServer; //采用MqttServerFactory对象的CreateMqttServer方法创建一个mqttServer 服务端
+                    obj_mqttServer.ApplicationMessageReceived += MqttServer_ApplicationMessageReceived;//消息接收处理事件
+                    obj_mqttServer.ClientConnected += MqttServer_ClientConnected;   //客户端连接处理事件
+                    obj_mqttServer.ClientDisconnected += MqttServer_ClientDisconnected;  //客户端断开连接处理事件
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+
+            obj_mqttServer.StartAsync();   //异步启动mqttServer
+            Console.WriteLine("MQTT服务启动成功！");
+        }
+
+        private static void MqttServer_ClientConnected(object sender, MqttClientConnectedEventArgs e)
+        {
+            Console.WriteLine($"客户端[{e.Client.ClientId}]已连接，协议版本：{e.Client.ProtocolVersion}");
+        }
+
+        private static void MqttServer_ClientDisconnected(object sender, MqttClientDisconnectedEventArgs e)
+        {
+            Console.WriteLine($"客户端[{e.Client.ClientId}]已断开连接！");
+        }
+
+        private static void MqttServer_ApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+        {
+            Console.WriteLine($"客户端[{e.ClientId}]>> 主题：{e.ApplicationMessage.Topic} 负荷：{Encoding.UTF8.GetString(e.ApplicationMessage.Payload)} Qos：{e.ApplicationMessage.QualityOfServiceLevel} 保留：{e.ApplicationMessage.Retain}");
+            Console.WriteLine("收到客户端消息");
+        }
+
+        private static void MqttNetTrace_TraceMessagePublished(object sender, MqttNetTraceMessagePublishedEventArgs e)
+        {
+            /*Console.WriteLine($">> 线程ID：{e.ThreadId} 来源：{e.Source} 跟踪级别：{e.Level} 消息: {e.Message}");
+            if (e.Exception != null)
+            {
+                Console.WriteLine(e.Exception);
+            }*/
+        }
+
+    }
+}
---
+++ 03.C#_client/MqttClientWin/FmMqttClient .Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace MqttClientWin
+{
+    partial class FmMqttClient
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSubTopic = new System.Windows.Forms.TextBox();
+            this.btnSubscribe = new System.Windows.Forms.Button();
+            this.txtReceiveMessage = new System.Windows.Forms.TextBox();
+            this.btnPublish = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPubTopic = new System.Windows.Forms.TextBox();
+            this.txtSendMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "订阅主题";
+            // 
+            // txtSubTopic
+            // 
+            this.txtSubTopic.Location = new System.Drawing.Point(133, 33);
+            this.txtSubTopic.Name = "txtSubTopic";
+            this.txtSubTopic.Size = new System.Drawing.Size(189, 25);
+            this.txtSubTopic.TabIndex = 1;
+            // 
+            // btnSubscribe
+            // 
+            this.btnSubscribe.Location = new System.Drawing.Point(345, 36);
+            this.btnSubscribe.Name = "btnSubscribe";
+            this.btnSubscribe.Size = new System.Drawing.Size(75, 23);
+            this.btnSubscribe.TabIndex = 2;
+            this.btnSubscribe.Text = "订阅";
+            this.btnSubscribe.UseVisualStyleBackColor = true;
+            this.btnSubscribe.Click += new System.EventHandler(this.BtnSubscribe_ClickAsync);
+            // 
+            // txtReceiveMessage
+            // 
+            this.txtReceiveMessage.Location = new System.Drawing.Point(63, 67);
+            this.txtReceiveMessage.Multiline = true;
+            this.txtReceiveMessage.Name = "txtReceiveMessage";
+            this.txtReceiveMessage.Size = new System.Drawing.Size(381, 167);
+            this.txtReceiveMessage.TabIndex = 3;
+            // 
+            // btnPublish
+            // 
+            this.btnPublish.Location = new System.Drawing.Point(158, 436);
+            this.btnPublish.Name = "btnPublish";
+            this.btnPublish.Size = new System.Drawing.Size(109, 41);
+            this.btnPublish.TabIndex = 4;
+            this.btnPublish.Text = "发布";
+            this.btnPublish.UseVisualStyleBackColor = true;
+            this.btnPublish.Click += new System.EventHandler(this.BtnPublish_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "发布主题";
+            // 
+            // txtPubTopic
+            // 
+            this.txtPubTopic.Location = new System.Drawing.Point(158, 293);
+            this.txtPubTopic.Name = "txtPubTopic";
+            this.txtPubTopic.Size = new System.Drawing.Size(286, 25);
+            this.txtPubTopic.TabIndex = 6;
+            // 
+            // txtSendMessage
+            // 
+            this.txtSendMessage.Location = new System.Drawing.Point(76, 337);
+            this.txtSendMessage.Multiline = true;
+            this.txtSendMessage.Name = "txtSendMessage";
+            this.txtSendMessage.Size = new System.Drawing.Size(368, 70);
+            this.txtSendMessage.TabIndex = 7;
+            // 
+            // FmMqttClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 489);
+            this.Controls.Add(this.txtSendMessage);
+            this.Controls.Add(this.txtPubTopic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnPublish);
+            this.Controls.Add(this.txtReceiveMessage);
+            this.Controls.Add(this.btnSubscribe);
+            this.Controls.Add(this.txtSubTopic);
+            this.Controls.Add(this.label1);
+            this.Name = "FmMqttClient";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSubTopic;
+        private System.Windows.Forms.Button btnSubscribe;
+        private System.Windows.Forms.TextBox txtReceiveMessage;
+        private System.Windows.Forms.Button btnPublish;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPubTopic;
+        private System.Windows.Forms.TextBox txtSendMessage;
+    }
+}
+
