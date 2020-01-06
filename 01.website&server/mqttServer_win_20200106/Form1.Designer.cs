38f108a8b966140a486def6b6381b367edc6052a
a-view <one_ouu@163.com>
2020-01-06 09:33:15 +0800

    mqtt服务器时间数据的修正
    解决了因为时间数据在插入数据库时未加引号而无效的bug

---
+++ 01.website&server/mqttServer_win_20200106/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace mqttServer_win
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tb_message = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tb_message
+            // 
+            this.tb_message.Location = new System.Drawing.Point(74, 12);
+            this.tb_message.Multiline = true;
+            this.tb_message.Name = "tb_message";
+            this.tb_message.Size = new System.Drawing.Size(520, 315);
+            this.tb_message.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_message);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_message;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
